ceda155db2748b441084980284af0edf1e30ca94
Nicolas PROCUREUR <nprocureur@gmail.com>
2015-05-20 03:13:00 +0200

    Update fake data in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#user1 = User.create(name: 'Arnold', password: 'azerty', mail: 'arnold@nodomain.com', lang: 'fr', country: 'fr', categories: [ 'movies', 'music' ])
-user1 = User.create(name: 'Arnold', lang: 'fr', country: 'fr', categories: [ 'movies', 'music' ])
+user1 = User.create(name: 'Arnold', lang: 'fr', country: 'fr', categories: [ 'movie', 'music' ], email: 'arnold@nodomain.tld', password: 'azertyui12', password_confirmation: 'azertyui12')
+user2 = User.create(name: 'Rosy', lang: 'en', country: 'en', categories: [ 'travel' ], email: 'rosy@nodomain.tld', password: 'rosytyui12', password_confirmation: 'rosytyui12')
 top1 = Top.create(title: 'Les meilleurs films en 2015', tags: ['#avengers2, #cinema, #science-fiction', '#divergente2'], category: 'movies', user: user1)
 Item.create(title: 'Avengers 2', top: top1)
 Item.create(title: 'Divergente 2', top: top1)
+
+
+top2 = Top.create(title: 'Les meilleurs voyages en 2014', tags: ['#evasion'], category: 'travel', user: user2)
+Item.create(title: 'Andorre', top: top2)
+Item.create(title: 'Carcassonne', top: top2)
